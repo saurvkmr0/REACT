a6a1dbfed04629e01b58aaa914ef35bab99e7b9e
saurvkmr0 <saurvkmr0@gmail.com>
2024-03-03 02:35:12 +0530

    tailwind configured

---
+++ dynamic_form/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Frame from './components/Frame';
+
+
+const App = () => {
+  return (
+    <div className='container bg-red-400'>
+        <Frame />
+    </div>
+  )
+}
+
+export default App;
---
+++ dynamic_form/src/components/Frame.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import wallpaper from '../assets/leaf color pattern2.jpg';
+
+const Frame = () => {
+  return (
+    <div className='Frame'>
+        <img src={wallpaper}/>
+    </div>
+  )
+}
+
+export default Frame
