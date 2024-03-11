5de245acd269d1fffda05f0f16dc384404a38e04
saurvkmr0 <saurvkmr0@gmail.com>
2024-03-11 22:51:25 +0530

    Dice game: added functionality of changing the scoreboard with the change of dice

--- dice_game/src/components/Content.jsx
+++ dice_game/src/components/Content.jsx
@@ -12,13 +12,20 @@ import Rules from './Rules'
 const diceArray=[dice0,dice1,dice2,dice3,dice4,dice5,dice6];
 var n=0;
 
-const Content = () => {
+const Content = ({ onDataReceived }) => {
     const[diceNum, setDice] = useState(n);
     function changeDice()
     {
         n=Math.floor(Math.random()*6)+1;
         console.log(n);
         setDice(n);
+        onDataReceived(n);
+    }
+
+    function resetScore()
+    {
+      console.log("reset");
+      onDataReceived(n+10);
     }
 
     
@@ -26,7 +33,7 @@ const Content = () => {
     <div className='flex items-center flex-col pt-12 gap-6'>
         <img className='dice' src={diceArray[n]} onClick={changeDice} />
         <p className='font-semibold text-2xl pb-5 '>Click on Dice to roll</p>
-        <Button buttonText="Reset Score" />
+        <Button buttonText="Reset Score" onClick={resetScore}/>
         <Button buttonText="Show Rules" />
         <Rules />
     </div>
--- dice_game/src/components/GamePage.jsx
+++ dice_game/src/components/GamePage.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ScoreBoard from './ScoreBoard';
 import Content from './Content';
 
 const GamePage = () => {
+
+  const [newScore, setNewScore] = useState("0");
+  function handelScoreData(data)
+  {
+    setNewScore(data);
+  }
+  
   return (
     <div className='mx-20 my-12'>
-      <ScoreBoard score="12" />
-      <Content />
+      <ScoreBoard score={newScore}/>
+      <Content onDataReceived={handelScoreData}/>
     </div>
   )
 }
