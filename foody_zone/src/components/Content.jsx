dccaa982b3170ac5b38a3b43c20987b214d9601f
saurvkmr0 <saurvkmr0@gmail.com>
2024-03-14 23:30:07 +0530

    server folder for data fetching is added

--- foody_zone/src/components/Content.jsx
+++ foody_zone/src/components/Content.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Card from './Card'
+import Data from './assets/Data'
 
 const Content = () => {
   return (
     <div className='content'>
         <Card />
     </div>
-  )
+  );
 }
 
 export default Content
---
+++ foody_zone/src/components/assets/Data.js
@@ -0,0 +1,20 @@
+const data = [
+    {
+      id: 1,
+      name: "John Doe",
+      age: 30
+    },
+    {
+      id: 2,
+      name: "Jane Smith",
+      age: 25
+    },
+    {
+      id: 3,
+      name: "Alice Johnson",
+      age: 35
+    }
+  ];
+  
+  export default data;
+  
