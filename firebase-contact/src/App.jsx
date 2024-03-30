45aa1328cc089fa75f5d61b2b50712567adcc576
saurvkmr0 <saurvkmr0@gmail.com>
2024-03-31 02:09:09 +0530

    in firebase proj logged firebase data and checked if the data is fetching correct or not form the firebase

--- firebase-contact/src/App.jsx
+++ firebase-contact/src/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './style.css';
 import Container from './components/Container';
+import { useEffect } from 'react'
+import { collection, getDocs } from 'firebase/firestore';
+import {db} from './config/firebase';
+
 
 
 const App = () => {
+
+  useEffect(()=>
+  {
+    const getContacts = async () =>
+    {
+      const contactsRef = collection(db,"contacts");
+      const contactsSnapshot = await getDocs(contactsRef);
+      console.log(contactsSnapshot);
+
+    }
+    getContacts();
+  }
+,[]);
+
+
   return (
     <div className="container max-w-full">
         <Container />
--- firebase-contact/src/screens/ContactScreen.jsx
+++ firebase-contact/src/screens/ContactScreen.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
 import NameCard from '../components/NameCard'
 
 const ContactScreen = () => {
+
   return (
     <div className="">
         <NameCard contactName="Saurav Kumar" ContactMail='saurvkmr0@.gmail.com'/>
