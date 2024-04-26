6dfd62be13d6aa3e78071e47cba6cda138c16101
saurvkmr0 <saurvkmr0@gmail.com>
2024-04-26 23:01:51 +0530

    crypto-dashboard tailwind working fix

---
+++ crypto-dashboard/src/Component/Container.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Container = () => {
+  return (
+    <div className='bg-red-400 text-2xl'>Container</div>
+  )
+}
+
+export default Container
---
+++ extras/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Container from './Component/Container';
+import './style.css'
+
+const App = () => {
+  return (
+    <Container/>
+  )
+}
+
+export default App
