08643aa2da62975a83e8b80b34175913819096fb
saurvkmr0 <saurvkmr0@gmail.com>
2024-04-26 23:21:50 +0530

    dashboard sidenav baisc structure done, styling needed

--- crypto-dashboard/src/Component/Container.jsx
+++ crypto-dashboard/src/Component/Container.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Container = () => {
   return (
-    <div className='bg-red-400 text-2xl'>Container</div>
+    <>
+      <aside className='w-[256px] h-screen flex flex-col justify-between px-[28px] py-[54px]'>
+      <header>
+        <h1>CRYPTOX</h1>
+        <div className='list-none py-6'>
+          <li className='h-[42px] flex items-center'>Dashboard</li>
+          <li className='h-[42px] flex items-center'>Transactions</li>
+        </div>
+      </header>
+      <footer className='list-none'>
+          <li className='h-[42px] flex items-center'>Support</li>
+      </footer>
+      </aside>
+    </>
   )
 }
 
