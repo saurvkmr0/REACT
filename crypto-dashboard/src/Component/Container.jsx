f154a112506456350bcb7ff032c1258953e0b35f
saurvkmr0 <saurvkmr0@gmail.com>
2024-05-01 01:32:14 +0530

    added navbar hidden and appering on different screen size using menu toggle button, still some refinment need to be done

--- crypto-dashboard/src/Component/Container.jsx
+++ crypto-dashboard/src/Component/Container.jsx
@@ -1,16 +1,30 @@
-import React,{useContext} from 'react'
+import React,{useContext, useState} from 'react'
 import SideNav from './SideNav';
 import Navbar from './Navbar';
 import Dashboard from '../Dashboard-comp/Dashboard';
 import { ToggleNavContext } from '../App';
 import Transaction from '../Transc-comp/Transaction';
 import Support from '../Support-comp/Support';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdArrowRoundBack } from "react-icons/io";
 
 const Container = () => {
   const [activeNav, setActiveNav] = useContext(ToggleNavContext);
+  const [sideNavIsOpen, setSideNavIsOpen] = useState(false);
+  
+  const toggleSideNavIcon=()=>{
+    setSideNavIsOpen(!sideNavIsOpen);
+  }
+
+  const toggleSideNav=()=>{
+    const sideNav = document.getElementById('sidenav');
+    sideNav.style.display==="block"?sideNav.style.display="none":sideNav.style.display="block";
+    toggleSideNavIcon();
+  }
   return (
     <div className='flex'>
-      <div><SideNav/></div>
+      <div id='sidenav' className='hidden lg:block'><SideNav/></div>
+      <div className='block lg:hidden p-8 font-bold text-2xl' onClick={toggleSideNav}>{!sideNavIsOpen?<GiHamburgerMenu />:<IoMdArrowRoundBack />}</div>
       <div className='grow'>
         <Navbar/>
         {activeNav==="dashboard"?
