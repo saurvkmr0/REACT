a1a59bca83b485bb6bfd124e5e83d99c4d2de0d1
saurvkmr0 <saurvkmr0@gmail.com>
2024-04-30 22:24:04 +0530

    added all the main pages basic structure and changing functionality, now all the components need to add

--- crypto-dashboard/src/Component/Container.jsx
+++ crypto-dashboard/src/Component/Container.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import SideNav from './SideNav';
 import Navbar from './Navbar';
+import Dashboard from '../Dashboard-comp/Dashboard';
+import { ToggleNavContext } from '../App';
+import Transaction from '../Transc-comp/Transaction';
+import Support from '../Support-comp/Support';
 
 const Container = () => {
+  const [activeNav, setActiveNav] = useContext(ToggleNavContext);
   return (
     <div className='flex'>
       <div><SideNav/></div>
       <div className='grow'>
         <Navbar/>
+        {activeNav==="dashboard"?
+        <Dashboard/>:
+        (activeNav==="transaction"?
+        <Transaction/>:
+        <Support/>)
+        }
       </div>
     </div>
   )
