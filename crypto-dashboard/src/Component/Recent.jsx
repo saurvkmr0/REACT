980f9f1d46320fd0460634b2196b871d3f45e477
saurvkmr0 <saurvkmr0@gmail.com>
2024-05-03 00:32:02 +0530

    In dashbord recent RecentTrans component completed

--- crypto-dashboard/src/Component/Recent.jsx
+++ crypto-dashboard/src/Component/Recent.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
 
-const Recent = () => {
+const Recent = ({icon,title,timestamp,amount1,amount2}) => {
   return (
-    <div>Recent</div>
+    <div className='w-[468px] pb-8 px-6'>
+        <div className="">
+            <div className="flex">
+                <div className='text-base p-3'>{icon}</div>
+                <div className="flex justify-between w-full">
+                    <div className="">
+                        <p className='text-base font-bold mb-1'>{title}</p>
+                        <p className='text-[#797E82] text-sm font-medium'>{timestamp}</p>
+                    </div>
+                    <div className='text-base font-bold text-end'>
+                        <p>{amount1}</p>
+                        <p className='text-[#797E82] text-sm font-medium'>{amount2}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- crypto-dashboard/src/Dashboard-comp/RecentTrans.jsx
+++ crypto-dashboard/src/Dashboard-comp/RecentTrans.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { AiFillDollarCircle } from "react-icons/ai";
+import { SiBitcoinsv } from "react-icons/si";
+import Recent from '../Component/Recent';
+
+
 
 const RecentTrans = () => {
+    const transactions=[
+        {
+            icon:<AiFillDollarCircle />,
+            title:"INR Deposit",
+            timestamp:"2022-06-09 7:06 PM",
+            amount1:"+ ₹81,123.10",
+            amount2:null,
+        },
+        {
+            icon:<SiBitcoinsv />,
+            title:"BTC Sell",
+            timestamp:"2022-05-27 12:32 PM",
+            amount1:"- 12.48513391 BTC",
+            amount2:"+ $81,123.10",
+        },
+        {
+            icon:<AiFillDollarCircle />,
+            title:"INR Deposit",
+            timestamp:"2022-06-09 7:06 PM",
+            amount1:"+ ₹81,123.10",
+            amount2:null,
+        },
+    ]
+
   return (
-    <div className='w-[468px] p-6'>
-        <div className="'text-gray-500 font-semibold">
+    <div className="">
+        <div className="'text-gray-500 font-semibold mb-6">
             Recent Transactions
         </div>
+        {transactions.map((item)=>
+            <Recent icon={item.icon} title={item.title} timestamp={item.timestamp} amount1={item.amount1} amount2={item.amount2}/>
+        )}
+        <div className="text-base font-bold text-center">View All</div>
     </div>
   )
 }
