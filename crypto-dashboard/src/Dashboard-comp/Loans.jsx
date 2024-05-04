3b46454d594f2bcbf03eb59248a68e636bda8f83
saurvkmr0 <saurvkmr0@gmail.com>
2024-05-04 21:48:57 +0530

    In dashbord recent Loans component completed

--- crypto-dashboard/src/Dashboard-comp/Loans.jsx
+++ crypto-dashboard/src/Dashboard-comp/Loans.jsx
@@ -3,8 +3,9 @@ import ButtonPill from '../Component/ButtonPill'
 
 const Loans = () => {
   return (
-    <div>
+    <div className='p-6 flex flex-col gap-4 w-[468px]'>
         <ButtonPill name="Loans" bgColor="#5F00D9" textColor="white"/>
+        <p className='font-medium text-lg'>Learn more about Loans – Keep your Bitcoin, access it’s value without selling it</p>
     </div>
   )
 }
